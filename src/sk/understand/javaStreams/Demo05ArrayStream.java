ca3d7d6e9045a4e643c20658161ddd9f12775160
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-01-29 10:56:25 -0500

    used Arrays.stream() with .filter() and regex matching

---
+++ src/sk/understand/javaStreams/Demo05ArrayStream.java
@@ -0,0 +1,32 @@
+/*
+ * Purpose : Learn about Arrays.stream() which can be used to generate a stream from Arrays. We can also use Stream.of()
+ * Link : https://www.youtube.com/watch?v=t1-YZ6bF-g0
+ * 
+ * all Stream are of the format
+ * "source + intermediateOperation + intermediateOperation + .... + intermediateOperation + TerminalOperation"
+ * 
+ * Date: 29-January-2019
+ */
+
+package sk.understand.javaStreams;
+
+import java.util.Arrays;
+
+public class Demo05ArrayStream {
+
+	public static void main(String[] args) {
+
+		System.out.println("-----Main Starts-----");
+
+		/* Generate a stream from list of strings */
+		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		
+		Arrays.stream(months)	// Same as Stream.of(months)
+//			.filter(x -> x.startsWith("M"))		// filters items starting with M
+			.filter(x -> x.matches(".*ber"))	// Matches the regex expression
+			.sorted()
+			.forEach(System.out::println);
+
+	}
+
+}
