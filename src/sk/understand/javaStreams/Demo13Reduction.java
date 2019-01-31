a7d8769433f88b530fe9d67cbbaf1e3158b351cc
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-01-30 23:08:37 -0500

    Use the reduction function with Doubles and Integers

--- src/sk/understand/javaStreams/Demo13Reduction.java
+++ src/sk/understand/javaStreams/Demo13Reduction.java
@@ -12,6 +12,7 @@
 package sk.understand.javaStreams;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Demo13Reduction {
@@ -21,17 +22,16 @@ public class Demo13Reduction {
 		System.out.println("-----Main Starts-----");
 
 		double dTotal = Stream.of(8.2, 6.7, 7.5, 6.2, 9.5)
-							  .reduce(0.0, (Double a, Double b) -> a + b);
-		
+							  .reduce(0.0, (Double a, Double b) -> a + b);		// We have to define a sum, where 'a' behaves like the aggregator
 		System.out.println("Total of Doubles : "+dTotal);
 		
 		
 		
 		
-		int iTotal = Stream.of(1,2,3,4,5,6,7)
-						   .reduce(0, (Integer i,Integer j) -> i+j);
-		
+		int iTotal = IntStream.of(1,2,3,4,5,6,7)		// intStream has an inbuilt function to reduce called sum 
+						   .sum();						// Builtin function to return sum
 		System.out.println("Total of Integers : "+ iTotal);
+		
 		
 		
 		
